cb45cd3f4620119de8362d61726016fc19352686
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-28 17:12:23 +0530

    Learnt about hibernate.cfg.xml file

--- hibernate-learnings/src/main/java/com/hari/hibernate_learnings/App.java
+++ hibernate-learnings/src/main/java/com/hari/hibernate_learnings/App.java
@@ -1,13 +1,24 @@
 package com.hari.hibernate_learnings;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Admin hari = new Admin();
+        hari.setId(10);
+        hari.setName("mass");
+        hari.setRole("head");
+        
+        Configuration con = new Configuration().configure();
+        
+        SessionFactory sf = con.buildSessionFactory();
+        
+        Session session = sf.openSession();
+        session.save(hari);
     }
 }
